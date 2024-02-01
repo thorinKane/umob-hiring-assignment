5197af5459ddf974fa24d5e73f669f037c6494b0
Thorin Kane <thorkane@amazon.com>
2024-02-01 13:57:07 -0800

    Clean up main activity

--- app/src/main/java/com/thorkane/playground/MainActivity.kt
+++ app/src/main/java/com/thorkane/playground/MainActivity.kt
@@ -7,12 +7,9 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.thorkane.playground.main.Main
 import com.thorkane.playground.ui.theme.PlaygroundTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,21 +31,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlaygroundTheme {
-        Greeting("Android")
     }
 }
