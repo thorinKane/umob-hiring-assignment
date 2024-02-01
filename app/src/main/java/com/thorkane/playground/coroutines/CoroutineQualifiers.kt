166deb1b796d0663605bac42135dc500319a2dad
Thorin Kane <thorkane@amazon.com>
2024-01-31 16:00:31 -0800

    Make Dispatchers injectable -- important for testing, helps keep me sane

---
+++ app/src/main/java/com/thorkane/playground/coroutines/CoroutineQualifiers.kt
@@ -0,0 +1,15 @@
+package com.thorkane.playground.coroutines
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IODispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
---
+++ app/src/main/java/com/thorkane/playground/coroutines/DispatcherModule.kt
@@ -0,0 +1,24 @@
+package com.thorkane.playground.coroutines
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DispatcherModule {
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IODispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
